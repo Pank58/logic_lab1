7acdcee1cfdac425998f8a4ff32a3dd90709db6b
--global <--global>
2024-08-29 23:41:50 +0300

    done lab1 num2

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -1,22 +1,14 @@
 ﻿#include <stdio.h>
-#include <limits.h>
+#include <stdlib.h>
 
 int main()
 {
-    int max=INT_MIN, min=INT_MAX, n = 10, i = 0;
+    int max=100, min=-100, n = 10, i = 0;
     int a[10];
 
     for (i; i < n; i++) {
-        a[i] = i * 4 + 3;
-        printf("%d ", a[i]);
+        a[i] = min+ rand()%(max-min+1);
+        printf("a[%d] = %d \ns",i ,a[i]);
     }
-
-    i = 0;
-    for (i; i < n; i++) {
-        if (a[i] > max) max = a[i];
-        if (a[i] < min) min = a[i];
-    }
-
-    printf("\n max-min=%d", max-min);
 }
 
