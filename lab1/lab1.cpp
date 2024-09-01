582967a687caa5f1d66dc358fa37157a9f797cf6
--global <--global>
2024-09-01 17:38:52 +0300

    revorc lab1 num5

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -4,21 +4,79 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct  {
+typedef struct student {
 	char Name[10];
 	char Famil[15];
-	int age, Nomzach;
-}student;
+	char age[10], Nomzach[15];
+	struct student* next;
+}Node;
 
-void searchNF(student list[], int c, const char* Name, const char* Famil) {
+int i = 1;
+char specchar = '/';
+
+Node* getLast(Node* head) {
+	if (head == NULL) {
+		return NULL;
+	}
+	while (head->next) {
+		head = head->next;
+	}
+	return head;
+}
+
+
+Node* input_head() {
+	Node* list = (Node*)malloc(sizeof(Node));
+	printf("name student Num%d:", i);
+	scanf("%s", list->Name);
+	printf("surname student Num%d:", i);
+	scanf("%s", list->Famil);
+	printf("age student Num%d:", i);
+	scanf("%s", list->age);
+	printf("number record book student Num%d:", i);
+	scanf("%s", list->Nomzach);
+	list->next = NULL;
+	return(list);
+}
+
+int input(Node* head) {
+	int f = 0;
+	Node* last = getLast(head);
+	Node* list = (Node*)malloc(sizeof(Node));
+	printf("name student Num%d:", i);
+	scanf("%s", list->Name);
+	printf("surname student Num%d:", i);
+	scanf("%s", list->Famil);
+	printf("age student Num%d:", i);
+	scanf("%s", list->age);
+	printf("number record book student Num%d:", i);
+	scanf("%s", list->Nomzach);
+	list->next = NULL;
+	last->next = list;
+	printf("Do you want to continue the list(+ - yes;/ - no)\n");
+	scanf(" %c", &specchar);
+	getchar();
+	if (specchar == '+') {
+		f = 1;
+	}
+	return(f);
+}
+
+
+
+void searchNF(Node* head, const char* Name, const char* Famil) {
 	int s = 0;
-	for (int i = 0; i < c; i++)
-	{
-		if (strcmp(list[i].Name, Name) == 0 && (strcmp(list[i].Famil, Famil) == 0)) {
-			printf("Result:%s %s age:%d, num: %d", list[i].Name, list[i].Famil, list[i].age, list[i].Nomzach);
-			break;
+	while (head != NULL) {
+		if (strcmp(head->Name, Name) == 0 && (strcmp(head->Famil, Famil) == 0)) {
+			printf("Result:%s %s age:%s, num: %s", head->Name, head->Famil, head->age, head->Nomzach);
 			s = 1;
+			break;
 		}
+		else
+		{
+			head = head->next;
+		}
+
 	
 	}
 	if (s != 1) {
@@ -26,39 +84,44 @@ void searchNF(student list[], int c, const char* Name, const char* Famil) {
 	}
 }
 
-void searchZ(student list[], int c, int Nomzach) {
+void searchZ(Node* head, const char* Nomzach) {
 	int s = 0;
-	for (int i = 0; i < c; i++)
-	{
-		if (Nomzach==list[i].Nomzach) {
-			printf("Result:%s %s age:%d, num: %d", list[i].Name, list[i].Famil, list[i].age, list[i].Nomzach);
+	while (head != NULL) {
+		if ((strcmp(head->Nomzach,Nomzach)==0)) {
+			printf("Result:%s %s age:%s, num: %s", head->Name, head->Famil, head->age, head->Nomzach);
 			s = 1;
 			break;
-			
 		}
+		else
+		{
+			head = head->next;
+		}
+
+
 	}
-	if (s == 0) {
+	if (s != 1) {
 		printf("Not found");
 	}
 }
 
 int main()
 {
-	char SName[10], SFamil[15];
-	int count = 0, sNomzach=0, t;
-	student st_list[10];
-	printf("Count stidents:");
-	scanf("%d", &count);
-	for (int i = 0; i < count; i++) {
-		printf("name student Num%d:", i + 1);
-		scanf("%s", st_list[i].Name);
-		printf("surname student Num%d:", i + 1);
-		scanf("%s", st_list[i].Famil);
-		printf("age student Num%d:", i + 1);
-		scanf("%d", &st_list[i].age);
-		printf("number record book student Num%d:", i + 1);
-		scanf("%d", &st_list[i].Nomzach);
+	char SName[10], SFamil[15], sNomzach[15];
+	int   t=0;
+	int f = 0;
+	Node* head = input_head();
+	printf("Do you want to continue the list(+ - yes;/ - no)\n");
+	scanf(" %c", &specchar);
+	getchar();
+	if (specchar == '+') {
+		f = 1;
 	}
+	while (f == 1)
+	{
+		i++;
+		f = input(head);
+	}
+
 
 	printf("1-search by name or 2-search by record book: ");
 	scanf("%d", &t);
@@ -67,12 +130,12 @@ int main()
 		scanf("%s", SName);
 		printf("input needed Surname:");
 		scanf("%s", SFamil);
-		searchNF(st_list, count, SName, SFamil);
+		searchNF(head, SName, SFamil);
 	}
 	else if (t == 2) {
 		printf("input needed number record book:");
-		scanf("%d", &sNomzach);
-		searchZ(st_list, count, sNomzach);
+		scanf("%s", sNomzach);
+		searchZ(head, sNomzach);
 	}
 	else
 	{
